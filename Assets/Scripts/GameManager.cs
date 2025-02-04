7542f3e5fb5c4812fe4f3a408b60453aa10c0247
MangoDraco <le303029@ucf.edu>
2025-02-04 17:38:31 -0500

    FixedUI
    UI is now colored and is navigable with just keyboard.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,80 +5,166 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
- public GameObject[] ghosts;
- public GameObject pacman;
- public Transform pellets;
+    public static GameManager Instance { get; private set; }
 
- public int score { get; private set;}
- public int lives { get; private set;}
+    [SerializeField] private Ghost[] ghosts;
+    [SerializeField] private PacMan pacman;
+    [SerializeField] private Transform pellets;
+    public int score { get; private set; } = 0;
+    public int lives { get; private set; } = 3;
 
-private void Start()
+    private int ghostMultiplier = 1;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
     {
         NewGame();
     }
 
     private void Update()
     {
-        if (this.lives <= 0 && Input.anyKeyDown)
+        if (lives <= 0 && Input.anyKeyDown)
         {
             NewGame();
         }
     }
+
     private void NewGame()
     {
         SetScore(0);
         SetLives(3);
         NewRound();
     }
+
     private void NewRound()
     {
-        foreach (Transform pellet in this.pellets)
+        
+
+        foreach (Transform pellet in pellets)
         {
             pellet.gameObject.SetActive(true);
         }
+
         ResetState();
     }
+
     private void ResetState()
     {
-        for (int i = 0; i < this.ghosts.Length; i++)
+        for (int i = 0; i < ghosts.Length; i++)
         {
-            this.ghosts[i].gameObject.SetActive(true);
+            ghosts[i].ResetState();
         }
-        this.pacman.gameObject.SetActive(true);
+
+        pacman.ResetState();
     }
+
     private void GameOver()
     {
-        for (int i = 0; i < this.ghosts.Length; i++)
+        
+
+        for (int i = 0; i < ghosts.Length; i++)
         {
-            this.ghosts[i].gameObject.SetActive(false);
+            ghosts[i].gameObject.SetActive(false);
         }
-        this.pacman.gameObject.SetActive(false);
+
+        pacman.gameObject.SetActive(false);
     }
-private void SetScore(int score)
-    {
-        this.score = score;
-    }
-private void SetLives(int lives)
+
+    private void SetLives(int lives)
     {
         this.lives = lives;
+        
     }
- public void GhostEaten(Ghost ghost)
-    {
-        SetScore(this.score + ghost.points);
-    }
-public void PacManEaten()
-    {
-        //insert dying animation here
-        this.pacman.gameObject.SetActive(false);
 
-        SetLives(this.lives - 1);
-        if(this.lives > 0)
+    private void SetScore(int score)
+    {
+        this.score = score;
+        
+    }
+
+    public void PacmanEaten()
+    {
+        pacman.DeathSequence();
+
+        SetLives(lives - 1);
+
+        if (lives > 0)
         {
-            Invoke(nameof(ResetState), 3.0f);
+            Invoke(nameof(ResetState), 3f);
         }
         else
         {
             GameOver();
         }
+    }
+
+    public void GhostEaten(Ghost ghost)
+    {
+        int points = ghost.points * ghostMultiplier;
+        SetScore(score + points);
+
+        ghostMultiplier++;
+    }
+
+    public void PelletEaten(Pellet pellet)
+    {
+        pellet.gameObject.SetActive(false);
+
+        SetScore(score + pellet.points);
+
+        if (!HasRemainingPellets())
+        {
+            pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3f);
+        }
+    }
+
+    public void PowerPelletEaten(PowerPellet pellet)
+    {
+        for (int i = 0; i < ghosts.Length; i++)
+        {
+            ghosts[i].frightened.Enable(pellet.duration);
+        }
+
+        PelletEaten(pellet);
+        CancelInvoke(nameof(ResetGhostMultiplier));
+        Invoke(nameof(ResetGhostMultiplier), pellet.duration);
+    }
+
+    private bool HasRemainingPellets()
+    {
+        foreach (Transform pellet in pellets)
+        {
+            if (pellet.gameObject.activeSelf)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ResetGhostMultiplier()
+    {
+        ghostMultiplier = 1;
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,69 +4,75 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 8.0f;
+    public float speed = 8f;
     public float speedMultiplier = 1f;
-
     public Vector2 initialDirection;
     public LayerMask obstacleLayer;
-    public new Rigidbody2D rigidbody {  get; private set; }
 
-    public Vector2 direction {  get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public Vector2 direction { get; private set; }
     public Vector2 nextDirection { get; private set; }
     public Vector3 startingPosition { get; private set; }
 
     private void Awake()
     {
-        this.rigidbody = GetComponent<Rigidbody2D>();
-        this.startingPosition = this.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
     }
 
     private void Start()
     {
-        
+        ResetState();
     }
 
-    public void RestartState()
+    public void ResetState()
     {
-        this.speedMultiplier = 1.0f;
-        this.direction = this.initialDirection;
-        this.nextDirection = Vector2.zero;
-        this.transform.position = this.startingPosition;
-        this.rigidbody.isKinematic = false;
-        this.enabled = true;
+        speedMultiplier = 1f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPosition;
+        rb.isKinematic = false;
+        enabled = true;
     }
 
     private void Update()
     {
-        if (this.nextDirection != Vector2.zero)
+        // Try to move in the next direction while it's queued to make movements
+        // more responsive
+        if (nextDirection != Vector2.zero)
         {
-            SetDirection(this.nextDirection);
+            SetDirection(nextDirection);
         }
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = this.rigidbody.position;
-        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
-        this.rigidbody.MovePosition(position + translation);
+        Vector2 position = rb.position;
+        Vector2 translation = speed * speedMultiplier * Time.fixedDeltaTime * direction;
+
+        rb.MovePosition(position + translation);
     }
 
     public void SetDirection(Vector2 direction, bool forced = false)
     {
+        // Only set the direction if the tile in that direction is available
+        // otherwise we set it as the next direction so it'll automatically be
+        // set when it does become available
         if (forced || !Occupied(direction))
         {
             this.direction = direction;
-            this.nextDirection = Vector2.zero;
+            nextDirection = Vector2.zero;
         }
         else
         {
-            this.nextDirection = direction;
+            nextDirection = direction;
         }
     }
 
     public bool Occupied(Vector2 direction)
     {
-        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacleLayer);
+        // If no collider is hit then there is no obstacle in that direction
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f, obstacleLayer);
         return hit.collider != null;
     }
 
